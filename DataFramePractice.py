cf345753f6e5edb8428dfc12e4a22ee2547ddbb7
Redbeard29 <ben@benshermanmusic.com>
2021-06-17 11:49:37 -0400

    more pandas practice

--- DataFramePractice.py
+++ DataFramePractice.py
@@ -17,3 +17,24 @@ simple_pp = pp[['pitcher', 'batter', 'i', 'o', 'b', 's', 'pitch_type', 'mph', 'm
 #doing basic math with data from our pp DataFrame:
 pp['sz_height'] = pp['sz_top'] - pp['sz_bot']
 pp_sz = pp[['pitcher', 'batter', 'i', 'o', 'b', 's', 'sz_top', 'sz_bot', 'sz_height']].head(10)
+
+#calculate distance of each pitch from middle of plate:
+pp['distance_from_middle_of_plate'] = np.abs(pp['x0'])
+
+#calculate the natura log^2 of spin rate:
+pp['ln_spin_rate'] = np.log(pp['spin_rate'])
+
+#add a constant 'season' variable throughout the entire DataFrame:
+pp['season'] = 2018
+
+#returning a random sample of data from our table:
+print(pp[['pitcher', 'batter', 'i', 'o', 'b', 's', 'pitch_type', 'mph', 'season']].sample(10))
+
+#creating a basic boolean column:
+pp['is_a_FF'] = (pp['pitch_type'] == 'FF')
+
+#using logical and and or with boolean columns:
+pp['is_a_FF_or_SL'] = (pp['pitch_type'] == 'FF') | (pp['pitch_type'] == 'SL')
+pp['fast_fastball'] = ((pp['pitch_type'] == 'FF') & (pp['mph'] >= 95))
+
+print(pp[['pitcher', 'pitch_type', 'mph', 'fast_fastball']].sample(5))
