1c9933b77462512c1400937e4d79a5aa6db44ab8
Redbeard29 <ben@benshermanmusic.com>
2021-06-21 09:50:40 -0400

    practice with functions

--- FunctionsOnDataFrames.py
+++ FunctionsOnDataFrames.py
@@ -21,3 +21,27 @@ print(pp['fast_4seam'].sum())
 print((pp['mph'] > 104).any())
 
 print((pp['mph'] >= 60).all())
+
+#practice calling boolean summary functions on individual players:
+print((at_bats[['H', 'SO']] > 200).any(axis=1).sample(10))
+
+#how many players in our dataset went for more than either 200 hits or 200 strikeouts:
+print((at_bats[['H', 'SO']] > 200).any(axis=1).sum())
+
+#how many players in our dataset went for more than BOTH 200 hits AND 200 strikeouts:
+print((at_bats[['H', 'SO']] > 200).all(axis=1).sum())
+
+#What about more than 150 hits and strikeouts:
+print((at_bats[['H', 'SO']] > 150).all(axis=1).sum())
+
+#how many of each type of pitch was thrown in the 2018 season?:
+print(pp['pitch_type'].value_counts())
+
+#same thing but as a percentage:
+print(pp['pitch_type'].value_counts(normalize=True))
+
+#how many times was each type of pitch thrown per inning:
+print(pd.crosstab(pp['i'], pp['pitch_type']).head(9))
+
+#now as percentages:
+print(pd.crosstab(pp['i'], pp['pitch_type'], normalize=True).head(9))
